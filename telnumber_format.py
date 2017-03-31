1ceb5ead78240898ac8bcbfef7c95ff73501d240
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2017-03-31 09:12:49 +0800

    Update telnumber_format.py

--- telnumber_format.py
+++ telnumber_format.py
@@ -6,7 +6,7 @@ f2 = open(r'F:\user\langyaofeng\Desktop\def.txt','a')
 f2.truncate()
 
 for i in f1:
-    m = re.sub(r'(\w+)',r"'\1',",i)
+    m = re.sub(r'(.+)',r"'\1',",i)
     f2.write(m)
 
 f2.close()
