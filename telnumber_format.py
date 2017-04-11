216b4ce55600a3f31638e07a6edd376f69becb3d
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2017-04-11 14:54:10 +0800

    Update telnumber_format.py

--- telnumber_format.py
+++ telnumber_format.py
@@ -1,13 +1,25 @@
 import re
 
-f1 = open(r'F:\user\langyaofeng\Desktop\abc.txt','r')
+f = open(r'F:\user\langyaofeng\Desktop\abc.txt','r')
+for y,x in enumerate(f):
+    pass
+y = y + 1
+f.close()
 
-f2 = open(r'F:\user\langyaofeng\Desktop\def.txt','a')
+f1 = open(r'F:\user\langyaofeng\Desktop\abc.txt','r')
+f2 = open(r'F:\user\langyaofeng\Desktop\def.txt','w')
 f2.truncate()
 
+count = 1
 for i in f1:
-    m = re.sub(r'(.+)',r"'\1',",i)
-    f2.write(m)
+    if y == count:
+        m = re.sub(r'(.+)',r"'\1'",i)
+        f2.write(m)
+    else:
+        m = re.sub(r'(.+)',r"'\1',",i)
+        f2.write(m)
+    count = count + 1
 
 f2.close()
 f1.close()
+
