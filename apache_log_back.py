26be82008525fb8fc1362d8d07e4f13ddc8d8577
xinxiaoyu <xinxiaoyu@users.noreply.github.com>
2017-09-07 17:23:57 +0800

    Create apache_log_back.py

---
+++ apache_log_back.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python env
+
+import os
+import shutil
+from datetime import date,timedelta
+
+today_time=date.today()
+http_log_path='/var/log/httpd/'
+
+for dirpath,dirnames,filenames in os.walk(http_log_path):
+    for filename in filenames:
+        if filename.endswith('log'):
+            if not os.path.isfile(http_log_path+filename+str(today_time)):
+                shutil.copy(http_log_path+filename,http_log_path+filename+str(today_time))
+                open(http_log_path+filename,'w').close()
+            else:
+                print '%s is aready exist!' % (http_log_path+filename+str(today_time))
